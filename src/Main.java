214b5b88d24bb5332f0839d357e70852b5edb51a
Nadir Emirbekov <Emirbekov93@mail.ru>
2021-10-26 21:35:12 +0300

    bank commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentBalance = 2000000000;
+        int transaction = 500000000;
+        int newBalance = currentBalance + transaction;
+        System.out.println(newBalance);
+    }
+}
